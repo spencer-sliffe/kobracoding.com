2a610b2946b9e2e7c9dbeb5457561eee7ab700c3
Spencer Sliffe <112651518+spencer-sliffe@users.noreply.github.com>
2023-01-09 10:59:22 -0600

    Update submit_1.php

--- HomePage/submit_1.php
+++ HomePage/submit_1.php
@@ -21,7 +21,7 @@ $subject.= $sub_3;
 $subject.= " applying for a ";
 $subject.= $sub_4;
 $subject.= " position";
-$message= $_POST['op_experience'];
+$message= $_POST['p_experience'];
 $headers = 'MIME-Version: 1.0' . "\r\n";
 $headers .= 'Content-type: text/html; charset=iso-8859-1' . "\r\n";
 $headers .= 'From:  ' . $sub_1. ' <' . $email .'>' . " \r\n" .
